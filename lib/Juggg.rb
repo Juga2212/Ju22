f0b724ed455b9652122af8748c6dc5037e9cae72
Juga2212 <160592181+Juga2212@users.noreply.github.com>
2024-03-03 01:09:26 +0200

    Update and rename seiji.rb to Juggg.rb

--- lib/Juggg.rb
+++ lib/Juggg.rb
@@ -1,10 +1,10 @@
-require "seiji/version"
+require "Juggg/version"
 
-module Seiji
+module Juggg
   class Response
 
     def call
-      puts "Hi, I'm Seiji. Ask me a 'yes or no' question about code, and I'll try to help you out! Type 'exit' to quit."
+      puts "Hi, I'm Juggg. Ask me a 'yes or no' question about code, and I'll try to help you out! Type 'exit' to quit."
       loop do
         question = gets.strip
         break if question == 'exit'
